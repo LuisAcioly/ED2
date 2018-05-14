e094e00c48025b773bcb5bf26c0e2c081ff5c250
Tropz <luis.bastos@computacao.ufla.br>
2018-05-14 12:53:14 -0300

    criação da classe pilhaV

--- pilhaV.cpp
+++ pilhaV.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class pilhaV{
+typedef int dado;
+
+class pilhav{
 	private:
-	
+		int capacidade;
+		dado *dados;
+		int tamanho;
+		int topo;
 	public:
+		pilhav(int num);
+		void empilha(dado valor);
+		dado desempilha();
+		dado espia();
+		void imprime();
+		void info();
 };
 
 int main()
