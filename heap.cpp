0c76e86a38103809d467a0010cc18feea3348a26
TropZZ <38045709+TropZZ@users.noreply.github.com>
2018-05-17 16:23:19 -0300

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,6 +1,8 @@
 #include "heap.hpp"
 #include <iostream>
+#include <cstdlib>
 
+using namespace std;
 maxHeap::maxHeap(int cap){
 	
 	heap = new data[cap];
@@ -11,13 +13,13 @@ maxHeap::maxHeap(int cap){
 
 void maxHeap::insere(data dado){
 	if(inseridos == capacidade){
-		cout >> "Erro ao inserir" >> endl;
+		cout << "Erro ao inserir" << endl;
 		exit(EXIT_FAILURE);
 	}
 	
 	heap[inseridos] = dado;
 	corrigeDescendo(inseridos);
-	tamanho++;
+	inseridos++;
 }
 
 void maxHeap::corrigeSubindo(int i){
@@ -39,9 +41,58 @@ int maxHeap::direito(int i){
 	return i*2+2;
 }
 
-void maxHeap::removeRaiz(){
+data maxHeap::removeRaiz(){
 	if(inseridos == 0){
 		cout << "Erro ao remover" << endl;
 		exit(EXIT_FAILURE);
 	}
+	int aux = heap[0];
+	swap(heap[0], heap[inseridos-1]);
+	inseridos--;
+	corrigeDescendo(0);
+	return aux;
+}
+
+void maxHeap::corrigeDescendo(int i){
+	int esq = esquerdo(i);
+	int dir = direito(i);
+	int maior;
+	
+	if(esq < inseridos and heap[esq] > heap[i]){
+		maior = esq;
+	}
+	else{
+		maior = i;
+	}
+	
+	if(dir < inseridos and heap[dir] > heap[maior]){
+		maior = dir;
+	}
+	
+	if(maior != i){
+		swap(heap[i], heap[maior]);
+		corrigeDescendo(maior);
+	}
+}
+
+data maxHeap::espiaRaiz(){
+	return heap[0];
+}
+
+void maxHeap::imprime(){
+	arruma();
+	
+	for (int i = 0; i < inseridos; i++)
+	{
+		cout << "[" << heap[i] << "]";
+	}
+	cout << endl;
+}
+
+void maxHeap::arruma(){
+	for (int i = inseridos/2-1; i >= 0; i--)
+	{
+		corrigeDescendo(i);
+	}
+	
 }
