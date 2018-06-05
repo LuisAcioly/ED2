7ae3a5af9e1e99c13df4586e671259534dd0c484
TropZZ <38045709+TropZZ@users.noreply.github.com>
2018-06-05 16:53:41 -0300

    Update hash3.cpp

--- hash3.cpp
+++ hash3.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
 typedef string dados;
 
-int funcaoHash(dados key, dados valor){
-    
+const int PRIMO = 13;
+
+int funcaoHash(dados key, int cap){
+    long h = 0;
+    for(int i = 0; i < key.length(); i++){
+        h = (PRIMO * h + key[i])%cap;
+    }
+    return h;
 }
 
 class noh{
+    friend class hash;
     private:
         noh* proximo;
         dados dado;
@@ -30,6 +39,7 @@ class hash{
     public:
         hash(int cap = 10);
         void insere(dados key, dados valor);
+        string recupera(dados key);
 
 };
 
@@ -40,4 +50,49 @@ hash::hash(int cap){
 
 void hash::insere(dados key, dados valor){
     int posicao = funcaoHash(dados key, dados valor);
+
+    if(this->recupera == "NÃO ENCONTRADO"){
+       if(elementos[h] == NULL){
+            elementos[h] = new noh;
+            elementos[h]->chave = key;
+            elementos[h]->dado = valor;
+            elementos[h]->proximo = NULL;
+       }
+       else{
+           cout << "colidiu" << endl;
+            noh* atual = elementos[h];
+           while(atual->proximo != NULL){
+               atual = atual->proximo;
+           }
+            noh* novo = new noh;
+            novo->chave = key;
+            novo->dado = valor;
+            atual->proximo = novo;
+           
+       }
+    }
+    
+}
+
+string hash::recupera(dados key){
+    long h = funcaoHash(dados key);
+
+    if(elementos[h] != NULL and elementos[h]->chave == key){
+        return elementos[h]->dado;
+    }
+    else{
+        noh* atual = elementos[h];
+
+        while(atual != NULL and atual->chave != key){
+                atual = atual->proximo;
+        }
+        if(atual != NULL and atual->chave == key){
+            return atual->valor;
+        }
+        else{
+            return "NÃO ENCONTRADO";
+        }
+
+
+    }
 }
